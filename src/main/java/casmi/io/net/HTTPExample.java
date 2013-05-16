160429c3b54a0dff7e86c8c943e225a9af3054dd
Takashi AOKI <federkasten@me.com>
2013-05-16 15:21:51 +0900

    initial commit

---
+++ src/main/java/casmi/io/net/HTTPExample.java
@@ -0,0 +1,49 @@
+/*
+ *   casmi examples
+ *   http://casmi.github.com/
+ *   Copyright (C) 2011, Xcoo, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package casmi.io.net;
+
+import java.io.IOException;
+
+import casmi.io.Reader;
+
+/**
+ * HTTP class example.
+ * <p>
+ * Gets public timeline of Twitter and shows.
+ * 
+ * @author T. Takeuchi
+ * 
+ * @see casmi.extension.net.HTTP
+ */
+public class HTTPExample {
+
+    public static void main(String[] args) {
+        HTTP http = null;
+        
+        try {
+            http = new HTTP("http://casmi.github.io");
+            Reader r = http.requestGet();
+            System.out.println(r.readAll());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+        	// do nothing
+        }
+    }
+}
